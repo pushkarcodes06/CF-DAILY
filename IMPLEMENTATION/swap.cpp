345a1339df3728b1398398d3c619d3191d05d7b4
PUSHKAR <pushkarrajishwar1@gmail.com>
2026-08-13 20:15:43 +0530

    Implement swap function in swap.cpp

---
+++ IMPLEMENTATION/swap.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n,m;
+    cin>>n>>m;
+    int t=n;
+    n=m;
+    m=t;
+    cout<<n<<" "<<m;   
+}
+int main(){
+    ios :: sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+    return 0;
+ }
