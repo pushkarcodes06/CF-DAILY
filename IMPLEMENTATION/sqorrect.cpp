2a9fe5c9df2c8ca1a49b4a93c114e8f4f67cd394
PUSHKAR <pushkarrajishwar1@gmail.com>
2026-08-16 22:09:17 +0530

    Add sqorrect.cpp to determine shape type

---
+++ IMPLEMENTATION/sqorrect.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+         int s,r;
+  cin>>r>>s;
+  if(s==r)
+  cout<<"Square"<<"\n";
+  else
+  cout<<"Rectangle"<<"\n";
+    }
+    
+    return 0;
+}
