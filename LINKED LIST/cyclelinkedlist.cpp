7c5986d07ad44d542729a11104b3fb3205c0ab46
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-08-31 20:54:29 +0530

    Create cyclelinkedlist.cpp

---
+++ LINKED LIST/cyclelinkedlist.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+         if(head == NULL || head->next == NULL)     //Gives 8ms
+        {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast != slow)
+        {
+            if(fast->next == NULL || fast->next->next == NULL)
+            {
+                return false;
+            }
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+    }
+};
