214758dec7c09983c3f4f2ee9a1bf4daabc8dff0
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-08-31 20:55:57 +0530

    Update cyclelinkedlist.cpp

--- LINKED LIST/cyclelinkedlist.cpp
+++ LINKED LIST/cyclelinkedlist.cpp
@@ -2,8 +2,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-
-         if(head == NULL || head->next == NULL)     //Gives 8ms
+         if(head == NULL || head->next == NULL)    
         {
             return false;
         }
