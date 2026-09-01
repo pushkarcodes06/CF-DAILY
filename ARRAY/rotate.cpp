a7dbd7e241a5caaf25eb3e30dc22b5c41fd58fde
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-09-01 22:32:53 +0530

    Create rotate.cpp

---
+++ ARRAY/rotate.cpp
@@ -0,0 +1,9 @@
+  class solution()
+{
+        k=k%nums.size();
+        reverse(nums.begin(),nums.begin()+(nums.size()-k));
+
+        reverse(nums.begin()+(nums.size()-k),nums.end());
+    
+        reverse(nums.begin(),nums.end());
+  }
