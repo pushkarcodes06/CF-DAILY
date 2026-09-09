9c8f05e9be0df3caf8fa66983210feed5caf2397
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-09-09 23:54:30 +0530

    Create calc.cpp

---
+++ IMPLEMENTATION/calc.cpp
@@ -0,0 +1,12 @@
+ 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a<<" + "<<b<<" = "<<a+b<<"\n";
+    cout<<a<<" * "<<b<<" = "<<a*b*1LL<<"\n";
+    cout<<a<<" - "<<b<<" = "<<a-b<<"\n";
+    
+    return 0;
+}
