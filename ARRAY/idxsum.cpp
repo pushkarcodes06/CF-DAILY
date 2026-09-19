2afc875c8eb7924588687f82c33dee33c2ec18bc
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-09-19 23:41:15 +0530

    Create idxsum.cpp

---
+++ ARRAY/idxsum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,q;
+    cin>>n>>q;
+    int a[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    while(q--){
+        int l,r;
+        cin>>l>>r;
+        long long sum =0;
+        for(int i=l-1;i<r;i++){
+            sum += a[i];
+        }
+
+        cout<<sum<<"\n";
+    }
+    return 0;
+}
