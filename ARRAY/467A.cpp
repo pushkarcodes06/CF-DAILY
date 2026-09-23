909aaa6ba69247317628ca209a4e2abb3c6f6a9e
PUSHKAR RAJ ISHWAR <pushkarrajishwar1@gmail.com>
2026-09-23 22:16:27 +0530

    Create 467A.cpp

---
+++ ARRAY/467A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n],b[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>a[i]>>b[i];
+    }
+   int ans =0;
+    for(int i=0;i<n;i++){
+        if((b[i]- a[i])>=2){
+            ans++;
+
+        }
+    }
+    cout<<ans;
+      
+    return 0;
+}
